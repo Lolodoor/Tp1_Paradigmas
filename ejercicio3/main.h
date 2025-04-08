074e894c292a3378d23c46b2dbc7fe261deff8ba
Lorenzo Door <lorenzodoor@MacBook-Pro-de-Lorenzo.local>
2025-04-08 16:57:45 -0300

    agrege main.h

---
+++ ejercicio3/main.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include "../ejercicio2/personajes/personaje.h"
+#include "../ejercicio2/armas/armas.h"
+#include "factory/personajeFactory.h"
+
+using namespace std;
+
+string nombreArma(shared_ptr<Arma> arma);
+string nombrePersonaje(shared_ptr<Personaje> personaje);
+vector<vector<int>> generarAleatorio();
+shared_ptr<Personaje> generarMagoAleatorio(pair<shared_ptr<Arma>, shared_ptr<Arma>> armas);
+shared_ptr<Personaje> generarGuerreroAleatorio(pair<shared_ptr<Arma>, shared_ptr<Arma>> armas);
+shared_ptr<Arma> generarArmaAleatoria();
